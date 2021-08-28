6973155c8fcbfc260483b5f7d1fdd914d11d2523
Alexander Zimin <klesh2@mail.ru>
2021-08-28 10:24:10 +0300

    eslint правки

--- src/view/no-event.js
+++ src/view/no-event.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import {createElement} from '../utils';
 
 const createNoEventTemplate = () => (
   '<p class="trip-events__msg">Click New Event to create your first point</p>'
